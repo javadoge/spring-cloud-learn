aef81708ee455016e4f835d52a08080ff8498ac2
sunil <linus@live.cn>
2018-01-15 10:51:14 +0800

    新增用户服务并实现负载均衡

---
+++ user-service-8082/src/main/java/com/javadoge/user/web/UserController.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2015-2020, www.javadoge.com (linus@live.cn).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.javadoge.user.web;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private Registration client;
+	
+	@GetMapping(value = "/user")
+	public String hello() {
+		return "hello user - "+client.getPort()+", local time=" + LocalDateTime.now();
+	}
+}
